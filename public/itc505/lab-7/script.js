4743b49969fdf79ce5624e2d681284ab07efa9eb
jaykuthadi-04 <jayanthika.kuthadi2001@gmail.com>
2024-11-17 20:46:17 -0700

    Add files via upload

---
+++ public/itc505/lab-7/script.js
@@ -0,0 +1,19 @@
+server.post('/itc505/lab-7', (req, res) => {
+    const { noun, verb, adjective, place, animal } = req.body;
+
+    if (!noun || !verb || !adjective || !place || !animal) {
+        res.send(`
+            <h1>Submission Failed</h1>
+            <p>Please fill out ALL fields.</p>
+            <a href="/itc05/lab-7">Go Back to Form</a>
+        `);
+        return;
+    }
+
+    const madLib = `Once upon a time in ${place}, there was a ${adjective} ${animal} who loved to ${verb} with a ${noun}.`;
+    res.send(`
+        <h1>Mad Lib Completed</h1>
+        <p>${madLib}</p>
+        <a href="/itc505/lab-7">Create Another Mad Lib</a>
+    `);
+});
